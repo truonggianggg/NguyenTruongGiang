b3789b43160d0a8713c97764a8eb4afbbc120ba0
truonggianggg <105715540+truonggianggg@users.noreply.github.com>
2022-08-25 09:41:32 +0700

    Up code

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = process.env.PORT | 4000;
+const port = process.env.PORT || 4000;
 
 app.listen(port,function(err){
     console.log("Server is running...");
